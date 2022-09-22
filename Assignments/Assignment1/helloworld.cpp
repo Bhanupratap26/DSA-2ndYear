81e45bed21d94bf24c39438b84991ca4fcb384f4
Bhanupratap26 <99680077+Bhanupratap26@users.noreply.github.com>
2022-09-22 08:32:34 +0530

    Update helloworld.cpp

--- Assignments/Assignment1/helloworld.cpp
+++ Assignments/Assignment1/helloworld.cpp
@@ -1,1 +1,11 @@
+//  HackerRanker - https://www.hackerrank.com/challenges/cpp-hello-world/submissions/code/290113620
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!";
+    return 0;
+}
 
