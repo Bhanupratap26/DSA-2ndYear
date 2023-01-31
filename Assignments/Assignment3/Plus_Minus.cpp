ff52b8223b55aea0572b893946728018ab2b81d5
Bhanupratap26 <99680077+Bhanupratap26@users.noreply.github.com>
2023-02-01 00:40:43 +0530

    Create Plus_Minus.cpp

---
+++ Assignments/Assignment3/Plus_Minus.cpp
@@ -0,0 +1,35 @@
+https://www.hackerrank.com/challenges/plus-minus/submissions/code/295114220
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void plusMinus(vector<int> arr) {
+    float count_positive = 0, count_negative = 0, count_zero = 0;
+    for (int i = 0; i < arr.size(); i ++){
+        if (arr[i] > 0){
+            count_positive++;
+        }
+        else if (arr[i] == 0){
+            count_zero++;
+        }
+        else {
+            count_negative++;
+        }
+    }
+    printf("%f\n%f\n%f", count_positive/arr.size(), count_negative/arr.size(), count_zero/arr.size());
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
