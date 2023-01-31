5ef0ded4bb9f3ee770d9b466b4a465736a3b74ce
Bhanupratap26 <99680077+Bhanupratap26@users.noreply.github.com>
2023-02-01 00:44:27 +0530

    Create Mini-Max_Sum.cpp

---
+++ Assignments/Assignment3/Mini-Max_Sum.cpp
@@ -0,0 +1,53 @@
+https://www.hackerrank.com/challenges/mini-max-sum/submissions/code/298749994
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void miniMaxSum(vector<int> arr) {
+    int i,j;
+    int long max=0;
+    int long min = 0;
+    
+   for(i=0;i<5;i++)
+   {
+       for(j=0;j<5;j++)
+       {
+       if(arr[j]<arr[j+1])
+       {
+           swap(arr[j],arr[j+1]);
+       }
+       }
+   }
+   
+ 
+ for(i=0;i<4;i++)
+ {
+     max=max+arr[i];
+ }
+ 
+ for(i=1;i<5;i++)
+ {
+     min=min+arr[i];
+ }
+cout<<min<<" ";
+cout<<max;
+
+}
+
+int main()
+{
+ 
+ int i,a;
+ vector<int>arr;
+ for(i=0;i<5;i++)
+ {
+     cin>>a;
+     arr.push_back(a);
+ }   
+   
+   miniMaxSum(arr);
+}
+
+
