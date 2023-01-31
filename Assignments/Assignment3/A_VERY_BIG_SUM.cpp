e60b24f339953b9ec7ec74c90c74a312531cf6f4
Bhanupratap26 <99680077+Bhanupratap26@users.noreply.github.com>
2023-02-01 00:36:10 +0530

    Create A_VERY_BIG_SUM.cpp

---
+++ Assignments/Assignment3/A_VERY_BIG_SUM.cpp
@@ -0,0 +1,28 @@
+https://www.hackerrank.com/challenges/a-very-big-sum/submissions/code/294563055
+
+
+#include <bits/stdc++.h>
+#include<vector>
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    
+    int i,n;
+    cin>>n;
+    long long A[n];
+    long long p;
+    for(i=0;i<n;i++)
+    {
+       cin>>A[i]; 
+    }
+    
+     for(i=0;i<n;i++)
+    {
+      p+= A[i];
+    }
+  
+  cout<<p;
+    
+}
