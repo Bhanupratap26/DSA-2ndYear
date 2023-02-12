fce041f158fa909b500ca9089ad29ec02a035597
Bhanupratap26 <99680077+Bhanupratap26@users.noreply.github.com>
2023-02-13 04:10:23 +0530

    Create in-order.cpp

---
+++ Assignments/Assignment-10/Implementation-using-stack/in-order.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans ;
+        stack<TreeNode *> S;
+        TreeNode *curr = root;
+
+        while(curr != NULL || S.empty() == 0) {
+            while (curr != NULL) {
+                S.push(curr);
+                curr = curr -> left;
+            }
+            curr = S.top();
+            ans.push_back(curr-> val);
+            S.pop();
+            curr = curr -> right;
+        }
+
+        return ans;
+
+        
+    }
+};
