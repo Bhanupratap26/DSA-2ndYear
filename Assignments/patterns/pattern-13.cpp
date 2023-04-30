b0aa0f90c420e2a12a02dee06b77b53fcf1d270e
Bhanupratap26 <99680077+Bhanupratap26@users.noreply.github.com>
2023-05-01 00:59:41 +0530

    Create pattern-13.cpp

---
+++ Assignments/patterns/pattern-13.cpp
@@ -0,0 +1,37 @@
+* * * *
+ * * *
+  * *
+   *
+   
+  
+  /******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin>>n;
+   for(int i = n; i>=0; i--)
+   {
+       for(int k = (n-i); k>0;k--)
+       {
+           cout<<" ";
+       }
+       for(int j = i;j>0;j--)
+           {
+               cout<<"* ";
+           }
+       cout<<"\n";
+   }
+
+    return 0;
+}
