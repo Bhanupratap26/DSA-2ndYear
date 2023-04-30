e5e7f47bb4b16a70b7bfd0d98e4ec9fa5b4e130b
Bhanupratap26 <99680077+Bhanupratap26@users.noreply.github.com>
2023-04-30 23:28:52 +0530

    Create pattern-1.cpp

---
+++ Assignments/patterns/pattern-1.cpp
@@ -0,0 +1,36 @@
+     #
+    ##
+   ###
+  ####
+ #####
+
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 5;
+   for(int i = 1 ; i<=n; i++)
+   {
+       for(int k = (n-i); k>0;k--)
+       {
+           cout<<" ";
+       }
+       for(int j = i;j>0;j--)
+           {
+               cout<<"#";
+           }
+       cout<<"\n";
+   }
+
+    return 0;
+}
